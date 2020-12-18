8a6c94dfd03e6d0ac078ccc9393332d058188859
Ivan Danci <bah4uk@gmail.com>
2020-12-18 23:00:15 +0100

    Solve day 2

---
+++ day2/part1.rb
@@ -0,0 +1,21 @@
+require 'pry-byebug'
+
+def password_valid?(line)
+  match_data = line.match(/^(?<min>\d+)\-(?<max>\d+)\s+(?<char>\D):\s+(?<password>.+)/)
+
+  (match_data[:min].to_i..match_data[:max].to_i).include?(match_data[:password].scan(match_data[:char]).size)
+end
+
+def solve
+  valid_passwords = 0
+
+  File.open("input.txt") do |f|
+    f.each_line.lazy.each do |line|
+      valid_passwords += 1 if password_valid?(line.chomp)
+    end
+  end
+
+  valid_passwords
+end
+
+puts solve
